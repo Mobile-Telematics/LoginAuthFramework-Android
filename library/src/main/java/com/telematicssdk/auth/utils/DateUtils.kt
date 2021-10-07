5b2b69ec3ee30cf4159a779d087b47ab3959472c
Ivan <bluetooth33d@gmail.com>
2021-10-07 16:16:37 +0300

    update checkDate

--- library/src/main/java/com/telematicssdk/auth/utils/DateUtils.kt
+++ library/src/main/java/com/telematicssdk/auth/utils/DateUtils.kt
@@ -7,6 +7,7 @@ internal object DateUtils {
 	fun checkDate(date: String): Boolean {
 		return try {
 			val format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.US)
+			format.parse(date)
 			true
 		} catch (e: Exception) {
 			false
